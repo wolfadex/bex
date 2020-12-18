2d1f3e342a936250f3a61559414dfbe804a8f0e9
wolfadex <wolfadex@gmail.com>
2020-12-18 09:06:57 -0700

    Load entry file

--- src/compiler.ts
+++ src/compiler.ts
@@ -6,29 +6,40 @@ const elmJS = decoder.decode(elmFile);
 const globalEval = eval;
 globalEval(elmJS);
 
-const buffer = new Uint8Array(1024);
 // @ts-ignore
-const app = globalThis.Elm.Compiler.init();
-
-interface ElmMsg {
-  action: string;
-  payload: any;
-}
-
-app.ports.toTS.subscribe(async function ({ action, payload }: ElmMsg) {
-  switch (action) {
-    case "LOAD_FILE":
-      const file = await Deno.readFile(payload);
-      app.ports.fromTS.send({
-        action: "FILE_LOADED",
-        payload: {
-          path: payload,
-          content: decoder.decode(file),
-        },
-      });
-      break;
-    case "WRITE_FILE":
-      await Deno.writeFile(payload.path, encoder.encode(payload.content));
-      break;
-  }
+const app = globalThis.Elm.Compiler.init({
+  flags: Deno.args,
 });
+
+app.ports.loadFile.subscribe(async function (filePath: string) {
+  const file = await Deno.readFile(filePath);
+  app.ports.fileLoaded.send({
+    path: filePath,
+    content: decoder.decode(file),
+  });
+});
+
+app.ports.status.subscribe(function (status: string) {
+  console.log(status);
+});
+
+// app.ports.toTS.subscribe(async function ({ action, payload }: ElmMsg) {
+//   switch (action) {
+//     case "LOAD_FILE":
+//       const file = await Deno.readFile(payload);
+//       app.ports.fromTS.send({
+//         action: "FILE_LOADED",
+//         payload: {
+//           path: payload,
+//           content: decoder.decode(file),
+//         },
+//       });
+//       break;
+//     case "WRITE_FILE":
+//       await Deno.writeFile(payload.path, encoder.encode(payload.content));
+//       break;
+//     case "STATUS":
+//       console.log(payload);
+//       break;
+//   }
+// });
